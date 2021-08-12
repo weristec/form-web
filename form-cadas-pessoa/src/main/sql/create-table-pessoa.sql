d231eb92f011aaf3a42f181ceccff9aa294f440d
weris <ribeirowelton10@gmail.com>
2021-08-12 09:31:21 -0300

    versao0

---
+++ form-cadas-pessoa/src/main/sql/create-table-pessoa.sql
@@ -0,0 +1,10 @@
+create table pessoa(
+id SERIAL,
+nome varchar(40),
+tel varchar(13),
+nasc varchar(11),
+email varchar(40),
+sexo varchar(2),
+tecnologia varchar(255),
+escolaridade varchar(20)
+);
---
+++ form-cadas-pessoa/src/main/sql/insert-table-pessoa.sql
@@ -0,0 +1,4 @@
+insert into pessoa(nome, tel, nasc, email, sexo, tecnologia, escolaridade) 
+values('Welton','6199458762','1993-07-25','cris1010@gmail.com','m','','');
+
+select *from pessoa p;
